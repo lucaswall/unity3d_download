59ecfe2d7b05c88bd1b38dd03a64ea6157dae839
Lucas Wall <wall.lucas@gmail.com>
2016-08-01 11:53:51 -0300

    Unity 5.4

--- download.sh
+++ download.sh
@@ -7,6 +7,8 @@ sh get_osx_ini.sh
 prefix="`cat prefix_url.txt`"
 ver="`cat version.txt`"
 
+wget -c "${prefix}CacheServer-${ver}.zip"
+
 wget -c "${prefix}MacEditorInstaller/Unity-${ver}.pkg"
 wget -c "${prefix}MacDocumentationInstaller/Documentation.pkg"
 wget -c "${prefix}MacStandardAssetsInstaller/StandardAssets-${ver}.pkg"
@@ -14,7 +16,7 @@ wget -c "${prefix}MacStandardAssetsInstaller/StandardAssets-${ver}.pkg"
 for f in `cat unity-${ver}-osx.ini | grep url= | grep UnitySetup | sed 's/url=//'` ; do wget -c "`cat prefix_url.txt`$f" ; done
 
 wget -c "${prefix}Windows64EditorInstaller/UnitySetup64-${ver}.exe"
-wget -c "${prefix}TargetSupportInstaller/UnitySetup-Windows-Support-for-Editor-${ver}.exe"
+#wget -c "${prefix}TargetSupportInstaller/UnitySetup-Windows-Support-for-Editor-${ver}.exe"
 wget -c "${prefix}TargetSupportInstaller/UnitySetup-Android-Support-for-Editor-${ver}.exe"
 wget -c "${prefix}TargetSupportInstaller/UnitySetup-iOS-Support-for-Editor-${ver}.exe"
 
--- install_all_osx.sh
+++ install_all_osx.sh
@@ -8,7 +8,7 @@ installer -pkg UnitySetup-Android-Support-for-Editor-${ver}.pkg -target /
 installer -pkg UnitySetup-AppleTV-Support-for-Editor-${ver}.pkg -target /
 installer -pkg UnitySetup-iOS-Support-for-Editor-${ver}.pkg -target /
 installer -pkg UnitySetup-Linux-Support-for-Editor-${ver}.pkg -target /
-installer -pkg UnitySetup-Mac-Support-for-Editor-${ver}.pkg -target /
+#installer -pkg UnitySetup-Mac-Support-for-Editor-${ver}.pkg -target /
 installer -pkg UnitySetup-Samsung-TV-Support-for-Editor-${ver}.pkg -target /
 installer -pkg UnitySetup-Tizen-Support-for-Editor-${ver}.pkg -target /
 installer -pkg UnitySetup-WebGL-Support-for-Editor-${ver}.pkg -target /
